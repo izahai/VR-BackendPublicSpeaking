646c13a74ed1c9f4ae597375b3689d93336b0356
izahai <ngynthhai@gmail.com>
2026-05-25 17:46:44 +0700

    fix cluster logic

--- utils/spliter.py
+++ utils/spliter.py
@@ -5,7 +5,7 @@ max_char_a_line = 46
 def split_text(
     text: str, 
     max_chars: int = max_char_a_line,
-    num_line_per_cluster : int = 5,
+    num_entries_per_cluster : int = 5,
     lines_per_entry: int = 2
 ) -> str:
     words = text.split()
@@ -26,15 +26,15 @@ def split_text(
 
     # --- Pair up the lines into 2-line entries ---
     paired_lines = []
-    for i in range(0, len(lines), lines_per_entry):
+    for i in range(0, len(lines)):
         # Join 2 lines with a newline character
         pair = " ".join(lines[i:i + lines_per_entry])
         paired_lines.append(pair)
     
     # --- Create clusters using the paired lines ---
     ls_cluster = []
-    for i in range(0, len(paired_lines), num_line_per_cluster):
-        cluster = paired_lines[i:i + num_line_per_cluster]
+    for i in range(0, len(paired_lines), num_entries_per_cluster):
+        cluster = paired_lines[i:i + num_entries_per_cluster]
         ls_cluster.append(cluster)
 
     # Write to file
